1fcc20fc5658e18db3c70f4b3ef8523fae9079fb
BragaMPedro <pedrobmagalhaes95@gmail.com>
2023-06-20 16:36:22 -0300

    feat infinite scroll

--- src/app/components/poke-list/poke-list.component.ts
+++ src/app/components/poke-list/poke-list.component.ts
@@ -8,29 +8,40 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss'],
 })
 export class PokeListComponent implements OnInit {
-
-
   private setAllPokemon!: PokeResult[];
   public allPokemon!: PokeResult[];
   public apiError: boolean = false;
 
   constructor(private service: PokeApiService) {}
 
-
   ngOnInit(): void {
-    this.service.getAllPokemon.subscribe( res => {
-      this.setAllPokemon = res;
-      this.allPokemon = this.setAllPokemon;
-    },
-    error => {
-      this.apiError = true
+    this.service.getAllPokemon.subscribe(
+      (res) => {
+        this.setAllPokemon = res;
+        this.allPokemon = this.setAllPokemon;
+      },
+      (error) => {
+        this.apiError = true;
+      }
+    );
+  }
+
+  onScroll() {
+    this.service.getMorePokemon.subscribe({
+      next: (res) => {
+        this.setAllPokemon.push(...res);
+        this.allPokemon = this.setAllPokemon;
+      },
+      error: (e) => {
+        this.apiError = true;
+      },
     });
   }
 
-  getSearch(value: string){
-    const filter = this.setAllPokemon.filter(res =>{
-      return !res.name.indexOf(value.toLowerCase())
-    })
+  getSearch(value: string) {
+    const filter = this.setAllPokemon.filter((res) => {
+      return !res.name.indexOf(value.toLowerCase());
+    });
     this.allPokemon = filter;
   }
 }
